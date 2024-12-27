a665619614d478748052f35df71416ead50ee0e0
Travis Stratton <stra0400@umn.edu>
2024-12-27 10:03:25 -0600

    Add instance_groups data source

---
+++ internal/provider/data_instance_groups.go
@@ -0,0 +1,136 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ datasource.DataSource = &InstanceGroupsDataSource{}
+
+func NewInstanceGroupsDataSource() datasource.DataSource {
+	return &InstanceGroupsDataSource{}
+}
+
+// InstanceGroupsDataSource defines the data source implementation.
+type InstanceGroupsDataSource struct {
+	client *AwxClient
+}
+
+// InstanceGroupsDataSourceModel describes the data source data model.
+type InstanceGroupsDataSourceModel struct {
+	Id types.String `tfsdk:"id"`
+}
+
+func (d *InstanceGroupsDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_instance_groups"
+}
+
+func (d *InstanceGroupsDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "Example data source",
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				MarkdownDescription: "Example identifier",
+				Required:            true,
+			},
+		},
+	}
+}
+
+func (d *InstanceGroupsDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	configureData := req.ProviderData.(*AwxClient)
+
+	d.client = configureData
+}
+
+func (d *InstanceGroupsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data InstanceGroupsDataSourceModel
+
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// set url for create HTTP request
+	id, err := strconv.Atoi(data.Id.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable convert id from string to int",
+			fmt.Sprintf("Unable to convert id: %v. ", data.Id.ValueString()))
+	}
+	url := d.client.endpoint + fmt.Sprintf("/api/v2/instance_groups/%d/", id)
+
+	// create HTTP request
+	httpReq, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to generate request",
+			fmt.Sprintf("Unable to gen url: %v. ", url))
+	}
+
+	httpReq.Header.Add("Content-Type", "application/json")
+	httpReq.Header.Add("Authorization", "Bearer"+" "+d.client.token)
+
+	httpResp, err := d.client.client.Do(httpReq)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to create example, got error: %s", err))
+	}
+	if httpResp.StatusCode != 200 {
+		resp.Diagnostics.AddError(
+			"Bad request status code.",
+			fmt.Sprintf("Expected 200, got %v. ", httpResp.StatusCode))
+
+	}
+
+	tmp := struct {
+		Id int `json:"id"`
+	}{}
+
+	defer httpResp.Body.Close()
+	httpRepsBodyData, err := io.ReadAll(httpResp.Body)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to get http response body",
+			fmt.Sprintf("Error was %v", err))
+		return
+	}
+	err = json.Unmarshal(httpRepsBodyData, &tmp)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to get unmarshall http response to grab ID",
+			fmt.Sprintf("error was %v", err))
+		return
+	}
+
+	idAsString := strconv.Itoa(tmp.Id)
+
+	data.Id = types.StringValue(idAsString)
+	// Write logs using the tflog package
+	// Documentation: https://terraform.io/plugin/log
+	tflog.Trace(ctx, "read a data source")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -122,7 +122,7 @@ func (p *awxProvider) Resources(ctx context.Context) []func() resource.Resource 
 
 func (p *awxProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
-		//NewExampleDataSource,
+		NewInstanceGroupsDataSource,
 	}
 }
 
