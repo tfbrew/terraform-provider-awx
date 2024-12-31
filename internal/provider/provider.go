ea765768928662687a65a43d725b17c71cddd7dc
Travis Stratton <stra0400@umn.edu>
2024-12-31 14:27:58 -0600

    Add workflow_job_templates resource.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -117,6 +117,7 @@ func (p *awxProvider) Resources(ctx context.Context) []func() resource.Resource 
 		NewJobTemplateInstanceGroupsResource,
 		NewNotificationTemplatesResource,
 		NewJobTemplateNotifTemplErrResource,
+		NewWorkflowJobTemplatesResource,
 	}
 }
 
