8a677e15512cae1f2f564b3bd2e195a4b73d317d
Travis Stratton <travis.stratton@gmail.com>
2025-01-23 21:24:44 -0600

    Created first version of MakeHTTPRequestToAPI in awxclient

--- internal/provider/awxclient.go
+++ internal/provider/awxclient.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 )
@@ -12,6 +13,30 @@ type AwxClient struct {
 	client   *http.Client
 	endpoint string
 	token    string
+}
+
+// A wrapper for http.NewRequestWithContext() that prepends tower endpoint to URL & sets authorization
+// headers and then makes the actual http request.
+func (c *AwxClient) MakeHTTPRequestToAPI(ctx context.Context, method, url string, body io.Reader) (*http.Response, error) {
+	url = c.endpoint + url
+	httpReq, err := http.NewRequestWithContext(ctx, method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	httpReq.Header.Add("Content-Type", "application/json")
+	httpReq.Header.Add("Authorization", "Bearer"+" "+c.token)
+
+	//TODO Add if body != nil, set body of http request
+
+	httpResp, err := c.client.Do(httpReq)
+	if err != nil {
+		return nil, err
+	}
+	if httpResp.StatusCode != 200 && httpResp.StatusCode != 404 {
+		return nil, fmt.Errorf("API call's HTTP status wasn't 200 or 404, was %vd", httpResp.StatusCode)
+	}
+
+	return httpResp, nil
 }
 
 func (c *AwxClient) AssocJobTemplChild(ctx context.Context, body ChildResult, url string) error {
