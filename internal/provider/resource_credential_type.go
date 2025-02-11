73c3e45bf946c9cd907fd1e1e1648bcb5739f8ef
alex-morford-desktop <alexjmorford@gmail.com>
2025-02-11 15:33:31 -0600

    test changes

--- internal/provider/resource_credential_type.go
+++ internal/provider/resource_credential_type.go
@@ -8,12 +8,14 @@ import (
 	"reflect"
 	"strconv"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
@@ -64,6 +66,9 @@ func (r *CredentialTypeResource) Schema(ctx context.Context, req resource.Schema
 				Optional:    true,
 				Computed:    true,
 				Description: "Either `cloud` or `net` but probably `cloud`.",
+				Validators: []validator.String{
+					stringvalidator.OneOf([]string{"cloud", "net"}...),
+				},
 			},
 		},
 	}
