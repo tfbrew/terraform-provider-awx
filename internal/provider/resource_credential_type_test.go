f524e626aa028e7fae2b81ef7145b427edfcf76d
alex-morford-desktop <alexjmorford@gmail.com>
2025-03-12 06:05:41 -0500

    feat(acc-testing): Add acceptance testing for credential_type resource and datasource

---
+++ internal/provider/resource_credential_type_test.go
@@ -0,0 +1,89 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+)
+
+func TestAccCredentialTypeResource(t *testing.T) {
+	resource1 := CredentialTypeAPIModel{
+		Name:        "test-credential-type-" + acctest.RandString(5),
+		Description: "test description 1",
+		Kind:        "cloud",
+	}
+	resource2 := CredentialTypeAPIModel{
+		Name:        "test-credential-type-" + acctest.RandString(5),
+		Description: "test description 2",
+		Kind:        "cloud",
+	}
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_1_0),
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCredentialTypeConfig(resource1),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"awx_credential_type.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(resource1.Name),
+					),
+					statecheck.ExpectKnownValue(
+						"awx_credential_type.test",
+						tfjsonpath.New("description"),
+						knownvalue.StringExact(resource1.Description),
+					),
+					statecheck.ExpectKnownValue(
+						"awx_credential_type.test",
+						tfjsonpath.New("kind"),
+						knownvalue.StringExact(resource1.Kind),
+					),
+				},
+			},
+			{
+				ResourceName:      "awx_credential_type.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccCredentialTypeConfig(resource2),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"awx_credential_type.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(resource2.Name),
+					),
+					statecheck.ExpectKnownValue(
+						"awx_credential_type.test",
+						tfjsonpath.New("description"),
+						knownvalue.StringExact(resource2.Description),
+					),
+					statecheck.ExpectKnownValue(
+						"awx_credential_type.test",
+						tfjsonpath.New("kind"),
+						knownvalue.StringExact(resource1.Kind),
+					),
+				},
+			},
+		},
+	})
+}
+
+func testAccCredentialTypeConfig(resource CredentialTypeAPIModel) string {
+	return fmt.Sprintf(`
+resource "awx_credential_type" "test" {
+  name         = "%s"
+  description  = "%s"
+}
+  `, resource.Name, resource.Description)
+}
