fa5f546a94f27c67d70cbd03ae34e671c501e323
Travis Stratton <stra0400@umn.edu>
2025-03-24 13:30:21 -0500

    feat(acc-testing): add test for resource_worfklow_job_template_node_label

---
+++ internal/provider/resource_workflow_job_template_node_label_test.go
@@ -0,0 +1,112 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+)
+
+func TestAccWkflwJobTemplJobNodeLabelResource(t *testing.T) {
+	IdCompare := &compareTwoValuesAsStrings{}
+	StringListCompare := &compareStringInList{}
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_1_0),
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccWkflwJobTemplJobNodeLabelResource1Config(),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.CompareValuePairs(
+						"awx_workflow_job_template_job_node.test1",
+						tfjsonpath.New("id"),
+						"awx_workflow_job_template_node_label.test",
+						tfjsonpath.New("id"),
+						IdCompare,
+					),
+					statecheck.CompareValuePairs(
+						"awx_label.test1",
+						tfjsonpath.New("id"),
+						"awx_workflow_job_template_node_label.test",
+						tfjsonpath.New("label_ids"),
+						StringListCompare,
+					),
+					statecheck.CompareValuePairs(
+						"awx_label.test2",
+						tfjsonpath.New("id"),
+						"awx_workflow_job_template_node_label.test",
+						tfjsonpath.New("label_ids"),
+						StringListCompare,
+					),
+				},
+			},
+			{
+				ResourceName:      "awx_workflow_job_template_node_label.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccWkflwJobTemplJobNodeLabelResource1Config() string {
+	return fmt.Sprintf(`
+resource "awx_organization" "test" {
+  name        = "%s"
+}
+resource "awx_inventory" "test" {
+  name         = "%s"
+  organization = awx_organization.test.id
+}
+resource "awx_project" "test" {
+  name         		= "%s"
+  organization 		= awx_organization.test.id
+  scm_type     		= "git"
+  scm_url      		= "git@github.com:user/repo.git"
+  allow_override 	= true
+}
+resource "awx_job_template" "test" {
+  name      				= "%s"
+  ask_inventory_on_launch 	= true
+  project   				= awx_project.test.id
+  playbook  				= "test.yml"
+}
+resource "awx_workflow_job_template" "test" {
+  name                     = "%s"
+  inventory                = awx_inventory.test.id
+  organization             = awx_organization.test.id
+}
+resource "awx_workflow_job_template_job_node" "test1" {
+  unified_job_template     	= awx_job_template.test.id
+  workflow_job_template_id 	= awx_workflow_job_template.test.id
+  inventory 				= awx_inventory.test.id
+}
+resource "awx_workflow_job_template_job_node" "test2" {
+  unified_job_template     	= awx_job_template.test.id
+  workflow_job_template_id 	= awx_workflow_job_template.test.id
+  inventory 				= awx_inventory.test.id
+}
+
+resource "awx_label" "test1" {
+	organization = awx_organization.test.id
+	name = "%s"
+}
+
+resource "awx_label" "test2" {
+	organization = awx_organization.test.id
+	name = "%s"
+}
+
+resource "awx_workflow_job_template_node_label" "test" {
+  id        = awx_workflow_job_template_job_node.test1.id
+  label_ids = [awx_label.test1.id, awx_label.test2.id]
+}
+  `, acctest.RandString(5), acctest.RandString(5), acctest.RandString(5), acctest.RandString(5), acctest.RandString(5), acctest.RandString(5), acctest.RandString(5))
+}
